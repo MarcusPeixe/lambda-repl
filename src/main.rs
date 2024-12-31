a76bf31ec98ca8c0fa7162133da4af972b6b9e1a
MarcusPeixe <marcusv.peixoto23@gmail.com>
2024-12-31 18:53:08 -0300

    feat: began working on parser (WIP) Must rethink parsing strategy and redo the types.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 enum ASTNode<'a> {
-    Variable(&'a str),
-    Application(Box<ASTNode<'a>>, Box<ASTNode<'a>>),
-    Abstraction(&'a str, Box<ASTNode<'a>>),
+    Var(&'a str),
+    Num(u64),
+    App(Box<ASTNode<'a>>, Box<ASTNode<'a>>),
+    Mul(Box<ASTNode<'a>>, Box<ASTNode<'a>>),
+    Div(Box<ASTNode<'a>>, Box<ASTNode<'a>>),
+    Add(Box<ASTNode<'a>>, Box<ASTNode<'a>>),
+    Sub(Box<ASTNode<'a>>, Box<ASTNode<'a>>),
+    Abs(&'a str, Box<ASTNode<'a>>),
 }
+
+type ParserInput<'a> = std::iter::Peekable<std::str::Chars<'a>>;
+
+struct ParserError<'a>(&'a str, String);
+
+type ParserResult<'a, T> = Result<(T, ParserInput<'a>), ParserError<'a>>;
+
+fn parse_whitespace(mut input: ParserInput) -> ParserResult<()> {
+    while let Some(&ch) = input.peek() {
+        if !ch.is_whitespace() {
+            break;
+        }
+    }
+    Ok(((), input))
+}
+
+// fn parse_identifier(mut input: ParserInput) -> ParserResult<>
 
 fn main() {
     println!("Hello, world!");
