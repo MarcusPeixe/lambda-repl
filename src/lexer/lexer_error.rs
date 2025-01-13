5f192831a46172de59710e16451800e08af20583
MarcusPeixe <marcusv.peixoto23@gmail.com>
2025-01-13 17:35:31 -0300

    feat: improved error printing

--- src/lexer/lexer_error.rs
+++ src/lexer/lexer_error.rs
@@ -2,44 +2,53 @@ use super::*;
 
 #[derive(Debug)]
 pub struct LexerError<'src> {
-    source: &'src str,
-    error: Option<Box<dyn std::error::Error>>,
     span: Span<'src>,
     message: String,
 }
 
 impl<'src> LexerError<'src> {
-    pub fn invalid_number(source: &'src str, span: Span<'src>, error: std::num::ParseIntError) -> Self {
-        let literal = span.get_text(source);
+    pub fn invalid_number(source: &'src source::Source, span: Span<'src>, details: &str) -> Self {
+        let literal = span.get_text(&source.text);
         Self {
-            source,
-            error: Some(Box::new(error)),
             span,
-            message: format!("invalid number literal \"{literal}\""),
+            message: format!("invalid number literal `{literal}` ({details})"),
         }
     }
 
-    pub fn invalid_symbol(source: &'src str, span: Span<'src>) -> Self {
-        let literal = span.get_text(source);
+    pub fn invalid_token(source: &'src source::Source, span: Span<'src>) -> Self {
+        let literal = span.get_text(&source.text);
         Self {
-            source,
-            error: None,
             span,
-            message: format!("invalid token \"{literal}\""),
+            message: format!("invalid token `{literal}`"),
         }
+    }
+
+    pub fn print(&self, f: &mut std::fmt::Formatter<'_>, tokens: &TokenVec) -> std::fmt::Result {
+        writeln!(f, "\x1B[1;31mLexer error:\x1B[39m {}\x1B[m", self.message)?;
+        writeln!(
+            f,
+            "   \x1B[1;34m-->\x1B[m {}",
+            self.span.get_location_str(tokens.source)
+        )?;
+        writeln!(f, "    \x1B[1;34m|\x1B[m")?;
+        self.span.print(f, tokens)?;
+        writeln!(f)
     }
 }
 
-impl<'src> std::fmt::Display for LexerError<'src> {
+pub type LexerResult<'src, T> = Result<T, LexerErrorVec<'src>>;
+
+#[derive(Debug)]
+pub struct LexerErrorVec<'src> {
+    pub tokens: TokenVec<'src>,
+    pub errors: Vec<LexerError<'src>>,
+}
+
+impl std::fmt::Display for LexerErrorVec<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "lexer error: {}", self.message)
+        for error in &self.errors {
+            error.print(f, &self.tokens)?;
+        }
+        Ok(())
     }
 }
-
-impl<'src> std::error::Error for LexerError<'src> {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        self.error.as_deref()
-    }
-}
-
-pub type LexerResult<'src, T> = Result<T, LexerError<'src>>;
