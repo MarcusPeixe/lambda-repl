f5ef0233cfd55690e53a6a5d9aba8eaac1d5776b
MarcusPeixe <marcusv.peixoto23@gmail.com>
2025-01-13 17:33:08 -0300

    feat: added filename to source objects

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -1,23 +1,32 @@
+#[derive(Debug)]
 pub struct Source {
+    pub file_name: String,
     pub text: String,
-    lines: Vec<usize>,
+    pub lines: Vec<usize>,
 }
 
 impl Source {
-    pub fn new(text: String) -> Source {
+    pub fn new(file_name: String, text: String) -> Source {
         let lines = find_lines(&text);
-        Source { text, lines }
+        Source {
+            file_name,
+            text,
+            lines,
+        }
     }
 
     // Returns line number/index, for the given offset
     pub fn get_line(&self, offset: usize) -> usize {
-        match self.lines.binary_search(&offset) {
-            Ok(idx) => idx,
-            Err(idx) => idx - 1,
-        }
+        self.lines.binary_search(&offset).unwrap_or_else(|i| i - 1)
     }
 
-    // Returns a slice of the line
+    pub fn get_line_column(&self, offset: usize) -> (usize, usize) {
+        let line = self.get_line(offset);
+        let start = self.lines[line];
+        let column = self.text[start..offset].chars().count();
+        (line, column)
+    }
+
     pub fn get_line_slice(&self, line: usize) -> &str {
         let start = self.lines[line];
         let &end = self.lines.get(line + 1).unwrap_or(&self.text.len());
@@ -28,7 +37,7 @@ impl Source {
 // Returns a vector of indices of line beginnings
 fn find_lines(text: &str) -> Vec<usize> {
     let mut lines = vec![0];
-    for (i, c) in text.chars().enumerate() {
+    for (i, c) in text.char_indices() {
         if c == '\n' {
             lines.push(i + 1);
         }
