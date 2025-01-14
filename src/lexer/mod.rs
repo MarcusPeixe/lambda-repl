fada1415de69504bcf64569011102b10236c6331
MarcusPeixe <marcusv.peixoto23@gmail.com>
2025-01-14 14:51:38 -0300

    refactor: renamed submodule

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,17 +1,17 @@
-mod lexer_error;
+mod error;
 mod span;
 mod token;
 mod token_vec;
 
 use crate::source;
 
-use lexer_error::*;
+use error::*;
 use token_vec::*;
 
-pub use lexer_error::LexerError;
+pub use error::LexerError;
 pub use span::Span;
 pub use token::{Token, TokenType};
-pub use token_vec::TokenVec;
+pub use token_vec::{TokenVec, TokenIter};
 
 pub fn tokenise(source: &source::Source) -> LexerResult<TokenVec> {
     TokenStream::new(source).tokenise()
