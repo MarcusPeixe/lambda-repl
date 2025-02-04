0e7cf9468ffa40d6d80f41a80ab38e01a7865a0b
MarcusPeixe <marcusv.peixoto23@gmail.com>
2025-02-04 17:23:06 -0300

    refactor: cmd args parsing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@ mod parser;
 mod source;
 
 fn main() {
-    let mut args: Vec<String> = std::env::args().collect();
-    println!("\nParser:\n");
+    let mut args = std::env::args();
 
     // Read command line arguments
     let file_name = args
-        .get_mut(1)
-        .expect("Error: expected source file name as first argument")
-        .clone();
+        .nth(1)
+        .expect("Error: expected source file name as first argument");
 
     // Read source code
     let source = source::Source::from_file(file_name).expect("Error: could not read source file");
@@ -24,6 +22,7 @@ fn main() {
         }
     };
 
+    // Parse
     let ast = match parser::parse(&tokens) {
         Ok(ast) => ast,
         Err(error) => {
